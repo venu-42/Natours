1f71381612bd8e3ef6a08b416d5db188c95fc46f
venu-42 <solipuram42@gmail.com>
2021-12-09 01:15:51 +0530

    Update ErrorController.js
    error handling seperated for dev and prod use

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,41 @@
+const sendErrorDev = (err,res)=>{
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message,
+        error:err,
+        errStack:err.stack
+    })
+}
+
+const sendErrorProd = (err,res)=>{
+    //* Type of errors we expected (user's wrong input, not found errors etc). these are solved using AppError class we created
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status:err.status,
+            message:err.message
+        })
+    }
+    //* programming or other unknown errors (logical errors etc) : shouldn't leak errors to public
+    else{
+        console.error(err);
+        
+        res.status(500).json({
+            status:'error',
+            message:'something really went wrong!'
+        })
+    }
+}
+
 module.exports = (err,req,res,next)=>{
     // console.log(err.stack)
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
-    res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message
-    })
+
+    if(process.env.NODE_ENV === 'development'){
+        sendErrorDev(err,res);
+    }
+    else if(process.env.NODE_ENV === 'production'){
+        sendErrorProd(err,res);
+    }
+    
 }
